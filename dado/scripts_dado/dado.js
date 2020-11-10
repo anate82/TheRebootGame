fe079084c6a349b139e826de5ef53f22d04a8f51
RominaElorrieta <rominaelorrieta@gmail.com>
2020-11-10 17:15:27 +0000

    Added CSS color scheme. Dice contains html, js and css files for animated dice implementation

---
+++ dado/scripts_dado/dado.js
@@ -0,0 +1,28 @@
+//Tira el dado
+function rollDice() {
+    const dice = [...document.querySelectorAll(".dice-list")];
+    dice.forEach(die => {
+      toggleClasses(die);
+      die.dataset.roll = getRandomNumber(1, 6); 
+      /*La función "dataset" nos permite acceder a los elementos personalizados como datos dentro del código HTML,
+      en éste caso, las caras del dado, que se han identificado dentro de los "li"*/
+    });
+  }
+  
+
+  //Hace el cambio de clases en el CSS durante la animación.
+  function toggleClasses(die) {
+    die.classList.toggle("odd-roll");//un solo dado
+    die.classList.toggle("even-roll");//dos dados
+  }
+  
+
+  //obtiene el número aleatorio en min y max. Los define entre 1 y 6 en la llamada 
+  //a la función principal.
+  function getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  document.getElementById("roll-button").addEventListener("click", rollDice); //evento en el botón.
--- scripts/game.js
+++ scripts/game.js
@@ -1,28 +1,51 @@
+/***********************************************
+ * PROYECTO FINAL BLOQUE 1.
+ *  -Ana Ángulo.
+ *  -Romina Elorrieta.
+ *  -Luis Sánchez.
+ * TÍTULO: "The Reboot Game"
+ * Inciado el 07/11/2020.
+ * Desarrollado en HTML5, CSS3 y JavaScript.
+ * 
+ * NOTA AL EQUIPO:
+ *      -"tba" es la abreviatura de 
+ * "to be added". Usar a discreción de cada uno.
+ *      -La inyección de código ballena nos 
+ * hace muy felices a todos!! Comentar ballenas
+ * a discreción de cada uno. ^___________^
+ * ********************************************/
+
 /*
-Objeto dice: genera una variable de un dado de "faces" caras con
+OBJETO DICE: genera una variable de un dado de "faces" caras con
 una función que retorna un número aleatorio entre 1 y 6
 */
 var Dice = function () {
     this.faces = 6;
+    //Aquí podemos implementar el dado animado.
     this.roll = function () {
         var randomNumber = Math.floor((Math.random()*this.faces)+1);
         return randomNumber;
     }
 };
 /* 
-Objeto player: genera una variable de un jugador con las caracterísitcas
+OBJETO PLAYER: genera una variable de un jugador con las caracterísitcas
 nombre del jugador, posicion en el tablero, contador interior 
-de movimientos, estado interior de actividad y color.
+de movimientos, estado interior de actividad y color. Características como
+el nombre ó el color, se recogen por entrada de teclado.
 */
 var Player = function (player) {
     this.name = "";
     this.posX = 100;   
     this.posY = 620;
     this.cell = 1;
-   //this.moveCount = 0;
-    this.color = "";
+   //this.moveCount = 0;--->tba
+   //this.color = "";--->tba
     this.active = false;
     this.meeple = document.getElementById(player);
+/*
+FUNCIONES DE MOVIMIENTO: pequeñas funciones propias del objeto Player que
+calculan el movimiento de la ficha en las cuatro direcciones en el tablero.
+*/
     this.moveRight = function () {
         this.posX += 160.8;
         this.meeple.style.left = this.posX + "px";
@@ -41,7 +64,7 @@ var Player = function (player) {
     }
 };
 /*
-Objeto Board: genera el tablero de juego con un array de 6x6
+OBJETO BOARD: genera el tablero de juego con un array de 6x6, un dado y un jugador.
 */
 var Board = function () {
     this.matrix = [];  
@@ -82,15 +105,10 @@ window.onload = function (){
             player1.cell++;
         },1000);
     }
-    
-
 }
 
 
-
-
-
-/*
+/*LO MÁS POSIBLE ES QUE NO LO NECESITEMOS
 switch(dado){
     case 1:mostrar cara 1;
     break;
