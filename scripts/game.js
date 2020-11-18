cd7c406277884a01feab5dbb495ad90eb2762707
ana <anate82@gmail.com>
2020-11-18 10:00:32 +0000

    Añadido evento pierde un turno y corregido errores en css de la visualizacion de los jugadores 3 y 4. Añadido en el dom los jugadores 3 y 4

--- scripts/game.js
+++ scripts/game.js
@@ -35,33 +35,6 @@ var Dice = function () {
         side5 : "assets/images/d5.png",
         side6 : "assets/images/d6.png"
     };
-<<<<<<< HEAD
-
-    this.animateDice = function (diceResult) {
-        var cont = 15;
-        var timerIdDice = setInterval (function () {
-            snd.play();
-            cont--;
-            let random = Math.floor((Math.random()*6)+1);
-            document.getElementById("diceImage").src = this.diceFaces['side'+random];
-            if (cont === 0) {
-                clearInterval(timerIdDice);
-                document.getElementById("diceImage").src = this.diceFaces['side'+diceResult];
-                if (diceResult === 1) {
-                    popup.message("Mueves "+ diceResult +" posicion!!!!");
-                    popup.show();
-                } else {
-                    popup.message("Mueves "+ diceResult +" posiciones!!!!");
-                    popup.show();
-                }
-                snd.pause();
-                //game.move(player,diceResult);
-                return;
-            }
-        }, 550);
-    }
-=======
->>>>>>> 059c0dcaf90543c487fb441c99a95767c1a7c28d
 };
 
 /* 
@@ -72,15 +45,12 @@ el nombre ó el color, se recogen por entrada de teclado.
 */
 
 var Player = function (meeple) {
-   // this.name = "";
     this.posX = 100;   
     this.posY = 620;
     this.cell = 1;
-    this.moveCount = 0;//--->tba
-    this.direction = 1;
-   //this.color = "";--->tba
-    this.active = false;
-    this.meeple = meeple;
+    this.direction = 1; //Indica el movimiento de la ficha, si es hacia adelante o hacia atrás
+    this.lostTurn = false;
+    this.meeple = meeple; //Es la ficha del tablero (jugador 1, jugador 2...)
     /*
     FUNCIONES DE MOVIMIENTO: funciones propias del objeto Player que
     calculan el movimiento de la ficha en las cuatro direcciones en el tablero
@@ -105,7 +75,7 @@ var Player = function (meeple) {
 };
 
 
-//clase constructora Game
+/* Clase constructora Game. Realiza la gestion de cambio de turno entre jugadores, gestiona la animación del movimiento del dado, realiza los movimientos de las fichas por el tablero y gestiona los eventos del tablero (mostrando popup y realizando movimientos)*/
 
 var Game = function (){
     this.dice = new Dice();
@@ -122,9 +92,9 @@ var Game = function (){
     this.rollDice = function() {
         document.getElementById("diceImage").style.display = "inline-block";
         var roll = this.dice.roll();
-        snd.play();
         let faceCont = 10;
         var diceTimer = setInterval(function (){
+            snd.play();
             faceCont--;
             let random = Math.floor((Math.random()*6)+1);
             document.getElementById("diceImage").src = this.dice.diceFaces['side'+random];
@@ -138,12 +108,11 @@ var Game = function (){
                     popup.message("Mueves "+ roll +" posiciones!!!!");
                     popup.show();
                 }
+                snd.pause();
             }
         },300);
-        //dice.animateDice(this,player,roll);
         return roll;//genera un valor aleatorio que será las posiciones a mover la ficha
     }
-
 
     this.move = function(player,diceRoll) {
         var snd = new Audio ("/assets/music/Astronomia.mp3");
@@ -193,11 +162,13 @@ var Game = function (){
     this.moveFivePosition = function (player) {
         this.move (player,5);
     }
+
     /*FUNCIÓN RUNEVENT: define las casillas en las cuales se producirá
     un evento en el tablero. En base a la casilla en la que caiga el
     jugador, se llamará a una función u otra, y se pueden avanzar ó 
     perder posiciones*/
     this.runEvent = function (player) {
+
         switch(player.cell) {
             case 6:
             case 12:popup.message("Has estado estudiando mucho, avanzas 2 posiciones para que te veas los recursos adicionales");
@@ -209,40 +180,38 @@ var Game = function (){
                     popup.show();
                     this.moveTwoBack (player);
                     break;
+
             case 24:
             case 34:popup.message("No has entregado el proyecto a tiempo!!!! Ohhh...vuelves a empezar el bootcamp");
                     popup.show();
                     this.startAgain (player);
                     break;
+
             case 17:popup.message("Nestor te ayuda con tus dudas y avanzas 5 posiciones!!");
                     popup.show();
                     this.moveFivePosition (player);
                     break;
-            case 28:popup.message("El hijo de un programador le pregunta a su padre: Papá, ¿Porque el sol nase por el este y se pone por el oeste?  El padre responde: Funciona? No se toca");
+
+            case 28:popup.message("El hijo de un programador le pregunta a su padre: Papá, ¿Porque el sol nase por el este y se pone por el oeste?  El padre responde:  Funciona? No se toca...");
                     popup.show();
                     break;
-            case 31:popup.message("Programador: Persona que resuelve un problema que tu no sabes que tienes, en una forma que tu no entiendes.");
+
+            case 31:popup.message("Programador(def.): Persona que resuelve un problema que tu no sabes que tienes, en una forma que tu no entiendes...");
                     popup.show();
                     break; 
             case 4:
             case 27:popup.message("Llegas tarde..... Te llevas un PUNISHER: Pierdes un turno por listo :(");
                     popup.show();
-                    this.moveCount = 1;
-                    this.active = false;
+                    player.lostTurn = true;
                     break;
-//******* Revisar pq el popup se cierra cuando se inserta la información en el input ****************/
             case 3:
             case 20:popupQuestion.messageQuestion("Responde a la pregunta, Rebooter: Piedra, papel, tijera, lagarto o ....");
-<<<<<<< HEAD
-                  popupQuestion.show();
-                  
-=======
-                   popupQuestion.show();
->>>>>>> 059c0dcaf90543c487fb441c99a95767c1a7c28d
-                   break;
+                    popupQuestion.show();   
+                    break;
         }
     }
 
+    /* Realiza el movimiento de avance en el tablero de juego de las fichas */
     this.moveOnBoard = function (player){
         if (player.cell >= 7 && player.cell < 12 || player.cell >= 25 && player.cell < 28 || player.cell >= 35 && player.cell < 36 ) {
             player.moveUp(player);
@@ -255,6 +224,7 @@ var Game = function (){
         }
     }
 
+    /* Realiza el movimiento de retroceso en el tablero de juego de las fichas */
     this.moveOnReverse = function (player) { 
         if (player.cell <= 33 && player.cell > 31 ||player.cell <= 23 && player.cell > 19 || player.cell <= 5 && player.cell > 0) {
             player.moveLeft(player);
@@ -267,41 +237,6 @@ var Game = function (){
         }
     }
 }
-<<<<<<< HEAD
-/*
-function checkTurn (){
-    if (player1.active){
-        console.log("player1 active false");
-        player1.active = false;
-        player2.active = true; 
-    } else {
-        console.log("player1 active true");
-        player1.active = true;
-        player2.active = false; 
-    }
-}
-
-function answerPopup(player){
-    if (document.getElementById("inputPopUp").value === "Spock") {
-
-        popup.message("Correctisimo!!!! Avanzas dos posiciones");
-        document.getElementById("inputPopUp").style.visibility = "hidden";
-        popup.show();
-        player.moveTwoPosition (player);
-        document.getElementById("btn-popup-close").innerText = "Cerrar";
-
-    } else {
-
-        popup.message("Ohhhhh!!!! Me había olvidado de que las personas normales tienen límites.... Retrocedes dos posiciones");
-        document.getElementById("inputPopUp").style.visibility = "hidden";
-        popup.show();
-        player.moveTwoBack (player);
-        document.getElementById("btn-popup-close").innerText = "Cerrar";
-
-    }
-}*/
-=======
->>>>>>> 059c0dcaf90543c487fb441c99a95767c1a7c28d
 
 /*OBJETO POPUP: define un objeto de tipo ventana emergente*/
 
@@ -334,20 +269,6 @@ var PopupQuestion = function (){
     }
 }
 
-/*
-OBJETO BOARD: genera el tablero de juego con un array de 6x6, un dado y un jugador.
-
-var Board = function () {
-    this.matrix = [];  
-    this.generateBoard = function () {
-        for ( var y = 0; y < 6; y++ ) {   
-            this.matrix[ y ] = [];    
-            for ( var x = 0; x < 6; x++ ) {        
-                this.matrix[ y ][ x ] = "";    
-            }
-        }
-    }
-};*/
 /*FUNCIÓN MOVEONBOARD: solventa los problemas de giro en el tablero. Se realiza
 el movimiento en base al intervalo de casillas en el que se encuentre el jugador*/
 
@@ -377,6 +298,8 @@ function moveOnReverse(player) {
         player.moveDown(player);
     }
 }
+
+/* Realiza el movimiento para el evento que se produce cuando se muestra el popup question */
 function moveEvent(type){
     if (game.activePlayer === 1){
         var p = player1;
@@ -392,30 +315,44 @@ function moveEvent(type){
     }
 }
 
+/*Función principal de movimiento de los jugadores. Gestiona los cambios de turno y realiza los movimientos*/
 function runTurn(diceResult) {
-   game.changeActivePlayer(); 
+    game.changeActivePlayer(); 
     if (game.activePlayer === 1){
-       game.move(player1,diceResult);
-   } else {
-        game.move(player2,diceResult);
+        if (player1.lostTurn){
+            game.move(player2,diceResult);
+            player1.lostTurn = false;
+            game.changeActivePlayer();
+        } else {
+            game.move(player1,diceResult);
+        }
+   } else if(game.activePlayer === 2){
+        if (player2.lostTurn){
+            game.move(player1,diceResult);
+            player2.lostTurn = false;
+            game.changeActivePlayer();
+        } else {
+            game.move(player2,diceResult);
+        }
    }
 }
 
 
 /*BUCLE PRINCIPAL DEL JUEGO*/
 
-
-var player1 = new Player(document.getElementById("player1"));
+// Obtenemos del dom los jugadores
+var player1 = new Player(document.getElementById("player1")); 
 var player2 = new Player(document.getElementById("player2"));
 var player3 = new Player(document.getElementById("player3"));
 var player4 = new Player(document.getElementById("player4"));
+
+//  A las fichas se les añade la posicion incial sobre el tablero
 player2.posX = 100;
 player2.posY = 580;
 player3.posX = 50;
 player3.posY = 580;
 player4.posX = 50;
 player4.posY = 620;
-player1.active = true;
 player1.meeple.style.top = player1.posY + "px";
 player1.meeple.style.left = player1.posX + "px";
 player2.meeple.style.top = player2.posY + "px";
@@ -424,47 +361,56 @@ player3.meeple.style.top = player3.posY + "px";
 player3.meeple.style.left = player3.posX + "px";
 player4.meeple.style.top = player4.posY + "px";
 player4.meeple.style.left = player4.posX + "px";
+
+// Creamos las variables de los objetos dado, juego y popup (mensajes y entrada informacion usuarios)
 var dice = new Dice();
-var popup = new Popup();
-var snd = new Audio("/assets/music/dice-1.wav");
+var snd = new Audio("/assets/music/dice-1.wav"); // audio del dado
 var game = new Game();
-var popupQuestion = new PopupQuestion();
+var popup = new Popup(); //Objeto popup para mensajes
+var popupQuestion = new PopupQuestion(); //Objeto popup para introducir por teclado respuesta del usuario
 
 window.onload = function (){
-    var diceButton = document.getElementById("diceButton");
-    var rollResult = 0;
-    var playerIdle = true;//nuestro jugador esta quieto
+    var diceButton = document.getElementById("diceButton"); // Obtenemos el elemento dado del dom
+    var rollResult = 0; // Contendrá el número de posiciones a moverse la ficha en el tablero de juego
+    var playerIdle = true; // playerIdle a true significa que nuestro jugador esta quieto (no es su turno de moverse por el tablero)
     diceButton.onclick = function () {
-        rollResult = game.rollDice();
-        playerIdle = false;
+        rollResult = game.rollDice(); //Genera un valor aleatorio
+        playerIdle = false; //el jugador se debe mover
     }
+
+    /*cierre del popup con mensaje desde la x*/
     document.getElementById("popup-close").addEventListener("click", function () {
-        if(!playerIdle){
+        if (!playerIdle) { //SI el jugador se debe mover entonces iniciamos el movimiento
             playerIdle = true;
             runTurn(rollResult);//lanza el dado y mueve la ficha hasta la posicion indicada por  el.
         }
         popup.close();
     });
+
+    /*cierre del popup con mensaje desde el boton*/
     document.getElementById("btn-popup-close").addEventListener("click", function (){
-        if(!playerIdle){
+        if (!playerIdle) { //SI el jugador se debe mover entonces iniciamos el movimiento
             playerIdle = true;
-            runTurn(rollResult);//lanza el dado y mueve la ficha hasta la posicion indicada por  el.
+            runTurn(rollResult); //lanza el dado y mueve la ficha hasta la posicion indicada por  el.
         }
         popup.close();
     });
+
+    /*Evento que recoge el click del popup con un input*/ 
     document.getElementById("btn-popup-confirmar").addEventListener("click", function (){
         popupQuestion.close();
         if (document.getElementById("inputPopUp").value === "Spock") {
             popup.message("Correctisimo!!!! Avanzas dos posiciones");
             popup.show(); 
-            moveEvent("forward");          
+            moveEvent("forward"); //funcion para realizar el movimiento de dos posiciones hacia adelante       
         } else {
             popup.message("Ohhhhh!!!! Me había olvidado de que las personas normales tienen límites.... Retrocedes dos posiciones");
             popup.show();
-            moveEvent("back");
+            moveEvent("back"); //fucnion para realizar el movimiento de dos posiciones hacia detrás
         }    
     });
-    
+
+    /*cierre del popup con input desde la x*/
     document.getElementById("popup-close-question").addEventListener("click", function () {
         popupQuestion.close();
     });
